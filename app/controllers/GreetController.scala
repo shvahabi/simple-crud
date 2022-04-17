b6130c0cd9b151c2aa2bbf7b665e63298fb55694
Shahed Vahabi <shvahabi@gmail.com>
2022-04-18 02:56:16 +0430

    🗃️ Connect to Database
    - Update dependencies for database connectivity
    - Add database connection configuration into `application.conf`
    - Add new endpoint `/greet/db`
    - Add sample query to `GreetController` for connection verification

--- app/controllers/GreetController.scala
+++ app/controllers/GreetController.scala
@@ -3,10 +3,37 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import slick.jdbc.PostgresProfile.api._
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 @Singleton
 class GreetController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   def greet(name: Option[String]) = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.greet(s"Hello ${name.getOrElse("Anonymous")}!"))
+  }
+
+  def dbGreet() = Action { implicit request: Request[AnyContent] => {
+    val db = Database.forConfig("db")
+    val query: Future[Vector[String]] = db.run {
+      sql"SELECT 'Database is configured properly ...'".as[(String)]
+    }
+    val timeout: Duration = 2.second
+    val result: Either[Exception, String] = Try {
+      Await.result(query, timeout)
+    } match {
+      case Success(s: Vector[String]) => Right(s.toList.head)
+      case Failure(e: Exception) => Left(e)
+    }
+    result match {
+      case Right(s: String) => Ok(views.html.greet(s))
+      case Left(t: Exception) => {
+        t.printStackTrace()
+        new Status(424)(views.html.greet("Unknown problem with database configuration ..."))
+      }
+    }
+  }
   }
 }

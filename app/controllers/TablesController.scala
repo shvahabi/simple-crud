d26abe4cb4af22c3587f2dddd80eb533df2624a2
Shahed Vahabi <shvahabi@gmail.com>
2022-04-20 03:43:26 +0430

    ♻️ Several Refactorings for More Play Idiomaticity
    - Refactor to conform to [slick's plain SQL queries](https://scala-slick.org/doc/3.3.3/sql.html)
    	- Add `case classes` for each query
    	- Introduce required `implicits`
    - Make `Action`'s `Result` a `Future`
    	- Make DB transaction error handling more play idiomatic
    - Make the html report more generic
    	- Accept arbitrary length `Lists` instead of `Tuple3`

--- app/controllers/TablesController.scala
+++ app/controllers/TablesController.scala
@@ -1,59 +1,69 @@
 package controllers
 
 import play.api.mvc._
-import play.twirl.api.HtmlFormat
 import slick.jdbc.GetResult
 import slick.jdbc.PostgresProfile.api._
 
 import javax.inject._
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.Future
 
 @Singleton
 class TablesController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def all(tableName: String) = Action { implicit request: Request[AnyContent] => {
+  def all(tableName: String) = Action.async { implicit request: Request[AnyContent] => {
+    import scala.concurrent.ExecutionContext.Implicits.global
     val db = Database.forConfig("db")
 
-    def query[T](queryString: String)(implicit getTResult: GetResult[T]): Either[Exception, Vector[T]] = {
-      val tryBlock: Try[Vector[T]] = Try {
-        Await.result(
-          db.run {
-            sql"#${queryString}".as[T]
-          }, 2.second)
+    def queryReport[T <: Product](queryString: String, heading: List[String])(implicit getResult: GetResult[T]): Future[Result] = {
+      val queryResult: Future[Vector[T]] = db.run {
+        sql"#${queryString}".as[T]
+      } andThen {
+        case _ => db.close()
       }
-      db.close()
-      tryBlock match {
-        case Success(s: Vector[T]) => Right(s)
-        case Failure(e: Exception) => Left(e)
-      }
-    }
-
-    def queryToResult[T](queryString: String, header: (String, String, String))(implicit getTResult: GetResult[T]): Result = {
-      query[T](queryString) match {
-        case Right(r: Vector[T]) => Ok(views.html.allRows(r.asInstanceOf[Vector[(Any, Any, Any)]], header))
-        case Left(e: Exception) => {
-          e.printStackTrace()
-          new Status(503)(views.html.userPrompt("Service unavailable, please try again in a while ..."))
+      queryResult.failed match {
+        case _: Future[NoSuchElementException] => queryResult map { x => Ok(views.html.allRows(x.map(y => y.productIterator.toList), heading)) }
+        case t: Future[Throwable] => {
+          t foreach { e => e.printStackTrace() }
         }
+          Future {
+            new Status(503)(views.html.userPrompt("Service unavailable, please try again in a while ..."))
+          }
       }
     }
 
     tableName match {
-      case "movies" => queryToResult[(Int, String, Int)]("SELECT id, title, year FROM movies", ("ردیف", "عنوان", "سال تولید"))
-      case "actors" => queryToResult[(Int, String, Int)]("SELECT id, name, birthday FROM actors", ("ردیف", "نام", "سال تولد"))
-      case "plays" => queryToResult[(String, String, String)](
-        s"""
-           |SELECT actors.name, plays.role, movies.title
-           |FROM actors
-           |INNER JOIN plays on actors.id = plays.actor
-           |INNER JOIN movies on movies.id = plays.movie
-           |WHERE actors.birthday > 1971
-           |ORDER BY actors.name ASC;
-           |""".stripMargin,
-        ("بازیگر", "نقش", "فیلم")
-      )
-      case _ => NotFound(views.html.notFound())
+      case "movies" => {
+        case class Movies(id: Int, title: String, year: Int)
+        implicit val getMovies = GetResult[Movies](r => Movies(r.nextInt, r.nextString, r.nextInt))
+        queryReport[Movies](
+          s"""
+             |SELECT id, title, year FROM movies
+             |""".stripMargin,
+          List("ردیف", "عنوان", "سال تولید"))
+      }
+      case "actors" => {
+        case class Actors(id: Int, name: String, birthday: Int)
+        implicit val getActors = GetResult[Actors](r => Actors(r.nextInt, r.nextString, r.nextInt))
+        queryReport[Actors](
+          s"""
+             |SELECT id, name, birthday FROM actors
+             |""".stripMargin,
+          List("ردیف", "نام", "سال تولد"))
+      }
+      case "plays" => {
+        case class Plays(name: String, role: String, title: String)
+        implicit val getPlays = GetResult[Plays](r => Plays(r.nextString, r.nextString, r.nextString))
+        queryReport[Plays](
+          s"""
+             |SELECT actors.name, plays.role, movies.title
+             |FROM actors
+             |INNER JOIN plays on actors.id = plays.actor
+             |INNER JOIN movies on movies.id = plays.movie
+             |WHERE actors.birthday > 1971
+             |ORDER BY actors.name ASC;
+             |""".stripMargin,
+          List("بازیگر", "نقش", "فیلم"))
+      }
+      case _ => Future { NotFound(views.html.notFound()) }
     }
   }
   }

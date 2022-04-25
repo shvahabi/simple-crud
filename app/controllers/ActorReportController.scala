409e9e2d1f03258e0c3febfa67e8e56760d9576c
Shahed Vahabi <shvahabi@gmail.com>
2022-04-25 15:04:35 +0430

    ♻️ Refactor for Better Rest API Naming
    - Add new routes with consistent naming
    	- Separate routes for all/single row(s) queries
    - Add new controller and view for the routes
    - Refactor into `package object controllers` to avoid code redundancy
    - Put models into dedicated `app/models` package

---
+++ app/controllers/ActorReportController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import slick.jdbc.PostgresProfile.api._
+import models.Actor
+
+@Singleton
+class ActorReportController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  def allActors() = Action.async { implicit request: Request[AnyContent] => {
+    implicit val db = Database.forConfig("db")
+    report[Actor](
+      query[Actor](
+        s"""
+           |SELECT * FROM actors;
+           |""".stripMargin
+      ),
+      result => Ok(views.html.someActors(result))
+    )
+  }
+  }
+
+  def singleActor(actorId: String) = Action.async { implicit request: Request[AnyContent] => {
+    implicit val db = Database.forConfig("db")
+    report[Actor](
+      query[Actor](
+        s"""
+           |SELECT * FROM actors
+           WHERE id = ${actorId};
+           |""".stripMargin
+      ),
+      result => Ok(views.html.someActors(result))
+    )
+  }
+  }
+}
---
+++ app/controllers/package.scala
@@ -0,0 +1,36 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.mvc.Result
+import play.api.mvc.Results.Status
+import slick.jdbc.{GetResult, PostgresProfile}
+import slick.jdbc.PostgresProfile.api._
+import models.Actor
+
+package object controllers {
+  implicit val getActors = GetResult[Actor](r => Actor(r.nextInt, r.nextString, r.nextInt))
+  implicit def ActorConverter[T](from: T): Vector[Actor] = from.asInstanceOf[Vector[Actor]]
+
+  def query[T <: Product](queryString: String)(implicit getResult: GetResult[T], db: PostgresProfile.backend.Database): Future[Vector[T]] = {
+    val queryResult: Future[Vector[T]] = db.run {
+      sql"#${queryString}".as[T]
+    } andThen {
+      case _ => db.close()
+    }
+    queryResult
+  }
+
+  def report[T <: Product](queryResult: Future[Vector[T]], result: Vector[T] => Result)(implicit db: PostgresProfile.backend.Database): Future[Result] = {
+    queryResult.failed match {
+      case _: Future[NoSuchElementException] => queryResult map { resolvedValue => result(resolvedValue) }
+      case throwable: Future[Throwable] => {
+        throwable foreach {
+          _.printStackTrace()
+        }
+      }
+        Future {
+          new Status(503)(views.html.userPrompt("Service unavailable, please try again in a while ..."))
+        }
+    }
+  }
+
+}

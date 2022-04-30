67e6cb3bed2c7f122970afdc9ea05108d404847d
Shahed Vahabi <shvahabi@gmail.com>
2022-04-30 13:58:57 +0430

    💄 Add Route/Controller/View for a landing Page

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -6,6 +6,13 @@ import javax.inject._
 import scala.concurrent.{ExecutionContext, Future}
 
 class IndexController @Inject()(controllerComponents: ControllerComponents)(implicit exec: ExecutionContext) extends AbstractController(controllerComponents) {
+  def landing(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] => {
+    Future {
+      Ok(views.html.navigation())
+    }
+  }
+  }
+
   def notFound(name: String): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] => {
     Future {
       new Status(404)(views.html.notFound())
